d409b2a1c586f7f49dac0e4fd5a710f80b879851
Kevin VF <knvfons20@gmail.com>
2025-07-30 01:25:15 +0200

    Border card animation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,16 +8,16 @@ function Dashboard() {
   return (
     <>
       <Hud isHome={true}/>
-      <div className="fixed inset-0 overflow-y-auto">
+      <div className="fixed inset-0 overflow-y-auto mb-8">
         <div className='flex flex-col gap-4 h-full m-5'>
           <div className='flex gap-4 h-1/3 w-full'>
-            <div className='bg-neutral-500 p-4 w-1/2 rounded-xl'></div>
-            <div className='bg-neutral-500 p-4 w-1/2 rounded-xl' ></div>
+            <div className='bg-neutral-800 p-4 w-1/2 card'></div>
+            <div className='bg-neutral-800 p-4 w-1/2 card' ></div>
           </div>
-          <div className="w-full min-h-[300px] bg-neutral-500 p-4 text-white rounded-xl"><ExampleChart></ExampleChart></div>
+          <div className="w-full min-h-[300px] p-4 text-white card"><ExampleChart></ExampleChart></div>
           <div className="flex h-1/3 gap-4 w-full">
-            <div className='bg-neutral-500 p-4 w-1/2 rounded-xl'></div>
-            <div className='bg-neutral-500 p-4 w-1/2 rounded-xl'></div>
+            <div className='bg-neutral-800 p-4 w-1/2 card'></div>
+            <div className='bg-neutral-800 p-4 w-1/2 card'></div>
           </div>
         </div>
       </div>
